d71ad5459cf9382642ddf2673ac51d481f338fe9
vard05 <a.a.arkhipov05@gmail.com>
2022-09-14 19:38:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,211 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const { Client, Authenticator } = require('minecraft-launcher-core');
+const launcher = new Client();
+const { app, BrowserWindow, ipcMain } = electron;
+const os = require('os');
+const fs = require('fs');
+const open = require('open');
+const request = require('request')
+
+let mainWindow;
+let loginWindow;
+
+//app.setAsDefaultProtocolClient('vortex-launcher');
+var minecraftPath = './minecraft'
+var MCCurrentVersionUrl = 'https://vortex-mc.site/downloads/vortex.zip'
+var authPath = path.join(app.getPath("appData"), "vortex-launcher\\Auth.json");
+var token = '00140bdcd0289272aeed6a5b03611d1125a83cf4b0b0d4bc1c95baa4a1708c74'
+try {
+var authJsonData = fs.readFileSync(authPath, "utf8")
+token = JSON.parse(authJsonData).token
+}
+catch{}
+
+try{token = process.argv.find((arg) => arg.startsWith('vortex-launcher://')).replace('vortex-launcher://', '').replace('/', '');}
+catch{}
+if (token != 'null'){
+    writeToken(token)
+}
+
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+}
+else {
+  app.on('second-instance', (e, argv) => {
+    if (process.platform !== 'darwin') {
+        try{token = argv.find((arg) => arg.startsWith('vortex-launcher://')).replace('vortex-launcher://', '').replace('/', '');}
+        catch{}
+        if (token != 'null'){
+            writeToken(token)
+        }
+    }
+
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+      mainWindow.reload();
+    }
+  });
+}
+
+function writeToken(token) {
+    var tokenJson = {
+        token: token
+    }
+    fs.writeFile(authPath, JSON.stringify(tokenJson), function(err, result) {
+        if(err) console.log('error', err);
+    });
+}
+
+ipcMain.handle('getDeviceRam', async (event, ...args) => {
+            const result = os.totalmem();
+            return result
+        })
+ipcMain.handle('getRoaming', async (event, ...args) => {
+            const result = app.getPath("appData");
+            return result
+        })
+ipcMain.handle('getToken', async (event, ...args) => {
+    return token
+})
+
+app.on('ready', function () {
+    ipcMain.on('CloseClick', () => CloseApp());
+    ipcMain.on('MinClick', () => MinApp());
+    ipcMain.on('OpenResources', () => OpenResources());
+    ipcMain.on('LogOut', () => LogOut());
+    ipcMain.on('Reload', () => Reload());
+    ipcMain.on('OpenBrowser', (event, arg) => open(arg));
+    ipcMain.on('LaunchClick', (event, arg) => LaunchMinecraft(arg));
+
+    mainWindow = new BrowserWindow({
+        width: 970,
+        minWidth: 970,
+        height: 575,
+        minHeight: 575,
+        show: false,
+        frame: false,
+        webPreferences: {
+            enableRemoteModule: true,
+            nodeIntegration: true,
+            contextIsolation: false,
+            webSecurity: false,
+            nodeIntegrationInSubFrames: true,
+            webviewTag: true,
+            nodeIntegration: true,
+            experimentalFeatures: true,
+        }
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'src/mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.webContents.on('did-finish-load', function () {
+        mainWindow.show();
+    });
+
+    mainWindow.removeMenu();
+    //mainWindow.webContents.openDevTools();
+});
+
+async function LaunchMinecraft(args) {
+    if (!fs.existsSync(minecraftPath)) {
+        console.log('download task was executed, starting download from '+MCCurrentVersionUrl+', to '+minecraftPath+'/minecraft.zip')
+        fs.mkdirSync(minecraftPath)
+        download(MCCurrentVersionUrl, minecraftPath+'/minecraft.zip', pfu, pfe, pff)
+    }
+}
+
+async function pfu(percents) {
+    console.log('downloading minecraft | '+percents)
+    mainWindow.webContents.send('percentUpdate', percents)
+}
+
+async function pfe() {
+    console.log('pizdec')
+    //fade percentage to 0
+    //display error message
+}
+
+async function pff() {
+    console.log('downloaded minecraft succesfully')
+    //checksum
+    //fade percentage to 0
+    //reload mainWindow
+}
+
+function Reload() {
+    mainWindow.reload();
+}
+
+function LogOut() {
+    if (token != 'null') {
+        fs.unlinkSync(authPath)
+        token = 'null'
+        mainWindow.reload();
+    }
+}
+
+function CloseApp() {
+    mainWindow.close();
+    try {
+        loginWindow.close();
+    }
+    catch { }
+}
+
+function MinApp() {
+    mainWindow.minimize();
+}
+
+function OpenResources() {
+    require('child_process').exec('start "" "minecraft\\resourcepacks"');
+}
+
+function download(url, path, percentFuncUpdate, percentFuncError, percentFuncFin) {
+
+    const file = fs.createWriteStream(path)
+    var receivedBytes = 0
+    var totalBytes = 0
+    var lastSended = -1
+    
+    request.get(url)
+    .on('response', (response) => {
+        if (response.statusCode == 200) {
+            totalBytes = response.headers['content-length']
+        }
+        else{
+            return
+        }
+    })
+    .on('data', (chunk) => {
+        receivedBytes += chunk.length
+        var perc = (receivedBytes/totalBytes*100).toFixed(1)
+        if (perc!=lastSended){
+            percentFuncUpdate(perc)
+            lastSended = perc
+        }
+    })
+    .pipe(file)
+    .on('error', (err) => {
+        fs.unlink(path)
+        percentFuncError()
+    });
+
+    file.on('finish', () => {
+        file.close()
+        percentFuncFin()
+    });
+
+    file.on('error', (err) => {
+        fs.unlink(path)
+        percentFuncError()
+    });
+}
---
+++ src/js/universalFunctions.js
@@ -0,0 +1,166 @@
+const { ipcRenderer } = require('electron')
+const fs = require("fs")
+const path = require('path')
+
+let currentRAM = 512
+var token = 'null'
+
+ipcRenderer.on('percentUpdate', function (evt, message) {
+    document.getElementById('percentage').style.width = message + '%'
+    console.log(message); // Returns: {'SAVED': 'File Saved'}
+});
+
+init()
+
+async function init(){
+    await GetToken()
+    //await getNickname()
+    await loadSettings()
+}
+
+async function loadSettings(){
+    var settings = await GetSettings()
+    currentRAM = settings.ram
+}
+
+async function getNickname() {
+    if (token != 'null'){
+        await $.ajax({
+            type : "POST",
+            url  : "https://vortex-mc.site/lapi/getNickname.php",
+            data : { access_token : token },
+            success: function(res){
+                document.getElementById("nickname").innerHTML = res
+            }
+        })
+    }
+}
+
+async function saveData(){
+    var settingsPATH = await GetSettingsPATH()
+    var settingsJSON = new Object()
+    settingsJSON.ram = currentRAM
+    var jsonString= JSON.stringify(settingsJSON)
+    fs.writeFileSync(settingsPATH, jsonString)
+}
+
+var settingsState = false;
+var serversState = false;
+
+async function Settings() {
+    var button = document.getElementById('sett')
+    if (!settingsState){
+        document.getElementById("servers").style.display = "none"
+        document.getElementById('serr').classList.remove('sett-active')
+        serversState = false;
+        button.classList.add('sett-active')
+        settingsState = true
+        var slider = document.getElementById("ramRange")
+        var output = document.getElementById("ramValue")
+        var allram = await ipcRenderer.invoke('getDeviceRam')
+        slider.value = currentRAM
+        output.textContent = currentRAM + 'mb'
+        slider.max = allram / 1048576
+        slider.oninput = function() {
+            currentRAM = this.value
+            output.textContent = this.value + 'mb'
+        }
+        document.getElementById("backdrop").classList.add("darkenBG")
+        document.getElementById("settings").style.display = "block"
+    }
+    else {
+        button.classList.remove('sett-active')
+        settingsState = false
+        MainWindow()
+    }
+}
+
+function MainWindow() {
+    saveData()
+    document.getElementById("backdrop").classList.remove("darkenBG")
+    document.getElementById("settings").style.display = "none"
+    document.getElementById("servers").style.display = "none"
+}
+
+async function Servers(){
+    var button = document.getElementById('serr')
+    if (!serversState){
+        document.getElementById("settings").style.display = "none"
+        document.getElementById('sett').classList.remove('sett-active')
+        settingsState = false;
+        button.classList.toggle('sett-active')
+        serversState = true
+        
+        document.getElementById("backdrop").classList.add("darkenBG")
+        document.getElementById("servers").style.display = "block"
+    }
+    else {
+        button.classList.toggle('sett-active')
+        serversState = false
+        MainWindow()
+    }
+}
+
+async function LogOut() {
+    ipcRenderer.send('LogOut')
+}
+
+async function GetSettings() {
+    var settingsPATH = await GetSettingsPATH()
+    try {
+      if (fs.existsSync(settingsPATH)) {
+        let settings = JSON.parse(fs.readFileSync(settingsPATH, "utf8"))
+        return settings
+      }
+      else {
+        return {ram: 512}
+      }
+    } catch(err) {
+      console.error(err)
+      return {ram: 512}
+    }
+}
+
+async function GetSettingsPATH() {
+    var roaming = await ipcRenderer.invoke('getRoaming')
+    var settingsPATH = path.join(roaming, "vortex-launcher\\LauncherSettings.json")
+    console.log(settingsPATH)
+    return settingsPATH
+}
+
+async function GetToken() {
+    token = await ipcRenderer.invoke('getToken')
+    if (token == 'null')
+        document.getElementById('play').innerHTML = 'войти'
+}
+
+async function CloseApp() {
+    await saveData()
+    ipcRenderer.send('CloseClick')
+}
+
+function MinApp() {
+    ipcRenderer.send('MinClick')
+}
+
+function openBrowser(url) {
+    ipcRenderer.send('OpenBrowser', url)
+}
+
+async function LaunchMinecraft() {
+    if (token != 'null'){
+        await saveData()
+        ipcRenderer.send('LaunchClick', currentRAM)
+    }
+    else {
+        ipcRenderer.send('Authorize')
+    }
+}
+
+function OpenResources() {
+    ipcRenderer.send('OpenResources')
+}
+
+function OpenMods() {
+    ipcRenderer.send('OpenMods')
+}
